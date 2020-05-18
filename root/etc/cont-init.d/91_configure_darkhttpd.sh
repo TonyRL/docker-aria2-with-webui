bef907d238c9907dbb37d098285d91d87fc07da2
Tony <TonyRL@users.noreply.github.com>
2020-05-19 04:34:10 +0800

    Update root files

--- root/etc/cont-init.d/91_configure_darkhttpd.sh
+++ root/etc/cont-init.d/91_configure_darkhttpd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bash
 
-dockerize -template /app/start_darkhttpd.sh:/config/start_darkhttpd.sh
+cp -n /app/start_darkhttpd.sh /config/start_darkhttpd.sh
 
 chmod +x /config/start_darkhttpd.sh
 
-chown -R app:users /config
+chown -R abc:abc /config
